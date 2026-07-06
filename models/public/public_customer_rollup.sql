26077b6355b974105622e274623857ad816a5285
sai-newday <sai.munuganti@newday.com>
2026-07-06 13:00:32 +0100

    dummy changes

--- models/public/public_customer_rollup.sql
+++ models/public/public_customer_rollup.sql
@@ -2,7 +2,8 @@ select
     customer_tier,
     count(*) as customer_count,
     avg(lifetime_value) as avg_lifetime_value,
-    avg(number_of_orders) as avg_orders_per_customer
+    avg(number_of_orders) as avg_orders_per_customer,
+    1 as temp
 from {{ ref('public_customer_tiers') }}
 group by 1
 
