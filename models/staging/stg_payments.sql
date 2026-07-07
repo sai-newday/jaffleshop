757e6fd8a1f60e10c1da157b75e8d20521f9f67c
sai-newday <sai.munuganti@newday.co.uk>
2026-07-07 13:07:00 +0100

    Update stg_payments.sql

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -1,6 +1,6 @@
 select
     id*100 as payment_id,
-    orderid*2 as order_id,
+    orderid*20 as order_id,
     paymentmethod as payment_method,
     amount / 100.0 as amount
 from {{ source('stripe', 'raw_payments') }}
