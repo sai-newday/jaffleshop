3598a3cd58f3fbb8645ddf6578cb18333c3133c3
sai-newday <sai.munuganti@newday.co.uk>
2026-07-07 13:08:36 +0100

    Refactor payment_id and order_id calculations

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -1,7 +1,7 @@
 select
-    id*100 as payment_id,
-    orderid*20 as order_id,
-    paymentmethod as payment_method,
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod||' ' as payment_method,
     amount / 100.0 as amount
 from {{ source('stripe', 'raw_payments') }}
 
