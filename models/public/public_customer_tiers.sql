37aeca1728ed6754e3d3ad39a3846ae669ba290d
sai-newday <sai.munuganti@newday.co.uk>
2026-07-07 12:45:28 +0100

    Add new column 'new_Col' to customer tiers query

--- models/public/public_customer_tiers.sql
+++ models/public/public_customer_tiers.sql
@@ -1,12 +1,11 @@
 select
     customer_id,
+    null as new_Col,
     case
         when lifetime_value >= 100 then 'vip'
-    
         when lifetime_value >= 40 then 'regular'
         else 'new'
     end as customer_tier,
     lifetime_value,
     number_of_orders
 from {{ ref('public_customers') }}
-
