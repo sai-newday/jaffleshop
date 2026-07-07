57add8c94617ed8724831a91182d8e1e12789268
sai-newday <sai.munuganti@newday.co.uk>
2026-07-07 12:19:49 +0100

    Add case for customer tiers based on lifetime value

--- models/public/public_customer_tiers.sql
+++ models/public/public_customer_tiers.sql
@@ -2,6 +2,7 @@ select
     customer_id,
     case
         when lifetime_value >= 100 then 'vip'
+    
         when lifetime_value >= 40 then 'regular'
         else 'new'
     end as customer_tier,
