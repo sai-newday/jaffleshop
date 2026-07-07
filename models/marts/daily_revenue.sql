44b0a703aa8fea0aafe7225ad525348ce4c1e0b9
sai-newday <sai.munuganti@newday.co.uk>
2026-07-07 13:45:25 +0100

    Update orders_count to multiply by 23

--- models/marts/daily_revenue.sql
+++ models/marts/daily_revenue.sql
@@ -5,7 +5,7 @@ with orders as (
 final as (
     select
         order_date,
-        count(order_id) as orders_count,
+        count(order_id)*23 as orders_count,
         count(distinct customer_id) as customers_count,
         sum(amount) as gross_revenue,
         avg(amount) as avg_order_value
