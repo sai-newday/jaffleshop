61bc7c61635172f45838a970da571c20f46a4785
sai-newday <sai.munuganti@newday.com>
2026-07-10 11:14:38 +0100

    test: Remove last_name and email columns from stg_customers for blast radius validation

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,7 +1,5 @@
 select
     id as customer_id,
-    first_name,
-    last_name,
-    email  -- Added email field for blast radius validation
+    first_name
 from {{ source('jaffle_shop', 'raw_customers') }}
 
