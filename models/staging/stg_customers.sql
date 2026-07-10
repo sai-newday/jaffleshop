db15b9b92d38d5960fe3c6a8098ff8a88cbffb56
sai-newday <sai.munuganti@newday.com>
2026-07-10 11:09:08 +0100

    test: Add email field to stg_customers for blast radius validation

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,6 +1,7 @@
 select
     id as customer_id,
     first_name,
-    last_name
+    last_name,
+    email  -- Added email field for blast radius validation
 from {{ source('jaffle_shop', 'raw_customers') }}
 
